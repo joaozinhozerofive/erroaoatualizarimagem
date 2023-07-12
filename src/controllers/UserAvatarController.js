9f16594b0a0b41edf0d17567776162bb8b35120f
joaozinhozerofive <124640745+joaozinhozerofive@users.noreply.github.com>
2023-07-11 21:31:41 -0300

    Add files via upload

---
+++ src/controllers/UserAvatarController.js
@@ -0,0 +1,29 @@
+const sqliteConnection = require("../database/sqlite");
+const DiskStorage = require("../providers/diskStorage");
+
+class UserAvatarController {
+  async update(request, response) {
+    const user_id = request.user.id;
+    const avatarFilename = request.file.filename;
+
+    const database = await sqliteConnection();
+    const diskStorage = new DiskStorage();
+
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
+
+    if (!user) {
+      throw new AppError("Somente usuários autenticados podem mudar o avatar", 401);
+    }
+
+    if (user.avatar) {
+      await diskStorage.deleteFile(user.avatar);
+    }
+
+    const filename = await diskStorage.saveFile(avatarFilename);
+    user.avatar = filename;
+    
+    return response.json(user);
+  }
+}
+
+module.exports = UserAvatarController;
